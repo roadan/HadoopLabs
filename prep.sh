ab5310e8b9728856f638114e7a9bef680f2a9bfc
Yaniv Rodenski <roadan@gmail.com>
2015-05-27 14:53:41 +1000

    added exercise 4 to lab 3

--- prep.sh
+++ prep.sh
@@ -1,19 +1,20 @@
-DATASET_FOLDER=/Users/roadan/Work/Courses/Hadoop-New/Labs/data/
+DATASET_FOLDER=/Users/roadan/Work/Courses/Hadoop-New/HadoopLabs/data/
 
-for i in {1901..1977}
+for i in {1904..1977}
 do
  #cd $DATASET_FOLDER/$i
 
  # Un-gzip each station file and concat into one file
- echo "reporter:status:Un-gzipping  $DATASET_FOLDER/$i" 
+ #echo "reporter:status:Un-gzipping  $DATASET_FOLDER/$i" 
  
- for file in  $DATASET_FOLDER/$i/*; do
-  gunzip -c $file >> $DATASET_FOLDER/$i/data.all
-  echo "reporter:status:Processed $file" 
- done
+ #for file in  $DATASET_FOLDER/$i/*; do
+ # gunzip -c $file >> $DATASET_FOLDER/$i/data.all
+ # echo "reporter:status:Processed $file" 
+ #done
 
- gzip $DATASET_FOLDER/$i/data.all 
- 
+ #gzip $DATASET_FOLDER/$i/data.all 
+ mv $DATASET_FOLDER/$i/data.all.gz $DATASET_FOLDER/flat/$i.gz
+ echo "reporter:status:flatten  $DATASET_FOLDER/flat/$i.gz"
 done
 
 	
